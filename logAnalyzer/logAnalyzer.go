f56fe74f033f8e46a8127a8d457c0b6250289a29
rachata.p <rachata.ptnn@gmail.com>
2024-03-09 17:40:21 +0700

    the scaffold is done

--- logAnalyzer/logAnalyzer.go
+++ logAnalyzer/logAnalyzer.go
@@ -6,27 +6,40 @@ import "log-analyzer/model"
 type ResultStats struct {
 	LogTypeCount        map[string]int
 	HTTPStatusCodeCount map[string]int
-	ResponseTime        ResponseTimeStat
+	RespTime            RespTimeStat
 	CalledHostCount     map[string]int
 	// TimeZoneCount       map[string]int, for now have no idea how can i know the zones
 }
 
-type ResponseTimeStat struct {
-	Summary      ResponseTimeSummary
-	EachRoutes   map[string]ResponseTimeEachRoutes
+type RespTimeStat struct {
+	Summary      AllRoutesRespTime
+	EachRoute    EachRouteRespTime
 	SlowReqRatio string
 }
 
-type ResponseTimeSummary struct {
+// calc in the end
+type AllRoutesRespTime struct {
 	Max float32
 	Min float32
 	Avg float32
 }
 
-type ResponseTimeEachRoutes struct {
+type EachRouteRespTime struct {
+	EachRouteRespTimes           map[string]EachRouteRespTimeSummary
+	EachRouteRespTimeCalculating map[string]EachRouteRespTimeCalculating
+}
+
+// calc in the end
+type EachRouteRespTimeSummary struct {
 	Max float32
 	Min float32
 	Avg float32
+}
+
+// calc every line processing
+type EachRouteRespTimeCalculating struct {
+	TotalLatency int
+	CalledCount  int
 }
 
 func AnalyzeLog() ResultStats {
@@ -45,10 +58,21 @@ func AnalyzeLog() ResultStats {
 func (r ResultStats) CalcStat(args model.Arguments) {
 	// 		r.LogTypeCount[args.LogType] += 1
 	// 		r.HTTPStatusCodeCount[args.HTTPStatusCode] += 1
-	// 		latency, err := getLatencyInMs(args.Latency)
-	//
-	//		args.Latency
-	//		args.Host
-}
 
-// func
+	// 		latency, err := getLatencyInMs(args.Latency)
+	//		route, err := getRoute(args.URI)
+	//		r.RespTimeStat.EachRoute.EachRouteRespTimeCalculating[route].TotalLatency += latency //TODO: i think i should shorten this
+	//      r.RespTimeStat.EachRoute.EachRouteRespTimeCalculating[route].CalledCount += latency
+
+	//      if r.RespTimeStat.EachRoute.EachRouteRespTimeSummary[route].min > latency {
+	//	      r.RespTimeStat.EachRoute.EachRouteRespTimeSummary[route].min = latency
+	//      }
+	// 		if r.RespTimeStat.EachRoute.EachRouteRespTimeSummary[route].max < latency {
+	// 			r.RespTimeStat.EachRoute.EachRouteRespTimeSummary[route].max = latency
+	// 		}
+	//      // avg would be calc in calcSummaryStat(...)
+
+	//		r.CalledHostCount[args.Host] += 1
+
+	// 		// TODO: calc TimeZoneCount
+}
--- validation/validation.go
+++ validation/validation.go
@@ -1,5 +1,29 @@
 package validation
 
+// i think latency should be 'number+ms' every time but i need to check
+// func getLatencyInMs(latencyStr string) (latencyInt int, error) {
+// 		split := strings.Split(latencyStr, "ms")
+//		if len(split) != 2 {
+//			return 0, errors.New("not found ms in latency")
+// 		}
+// 		latency, err := strconv.Atoi(split[0])
+//		if err!= nil {
+// 			return 0, err
+// 		}
+//		return latency, nil
+// }
+
+// wanna put this func in package logAnalyzer, anyway i nid to validate if URI is valid
+// func getRoute(uri string) (route string, err error) {
+//	ex URI "URI": "/api/v1/payment/YZa3LRPteb1ANvDWlwb65c8GbTvPbogE"
+// 	split := strings.Split(uri, "/")
+//  if len(split) < 3 {
+// return "", errors.New("URI has less than 3 parts")
+// }
+// route = split[2] // need to check the correct index
+// 	return route, nil
+// }
+
 // assume log file is not a perfect file
 // maybe all LogSummary is not be placed in the odd line
 
@@ -16,7 +40,7 @@ package validation
 // LogSummary
 // LogSummary
 
-// TODO:
+// this func only use for check the soure file
 // func is rawLogPerfect() bool {
 // the first idea, LogSummary doesnt have many fields..
 // i choose 'status' for validation if log detail is placed in the odd line
@@ -31,16 +55,3 @@ package validation
 // }
 
 // TODO: maybe i should check if found logSummary, LogDetail 2 or 3 times in a row
-
-// i think latency should be 'number+ms' every time but i need to check
-// func getLatencyInMs(latencyStr string) (latencyF32 float32, error) {
-// 		split := strings.Split(latencyStr, "ms")
-//		if len(split) != 2 {
-//			return 0, errors.New("not found ms in latency")
-// 		}
-// 		latency, err := strconv.ParseFloat(split[0], 32)
-//		if err!= nil {
-// 			return 0, err
-// 		}
-//		return float32(latency), nil
-// }
